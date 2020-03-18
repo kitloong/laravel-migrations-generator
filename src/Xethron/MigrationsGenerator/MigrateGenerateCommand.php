d5d60ce68efece0b890260050653f4e0ecbb2738
kitloong <kitloong1008@hotmail.com>
2020-03-18 23:55:48 +0800

    Use Str

--- src/Xethron/MigrationsGenerator/MigrateGenerateCommand.php
+++ src/Xethron/MigrationsGenerator/MigrateGenerateCommand.php
@@ -1,5 +1,6 @@
 <?php namespace Xethron\MigrationsGenerator;
 
+use Illuminate\Support\Str;
 use Way\Generators\Commands\GeneratorCommand;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
@@ -324,7 +325,7 @@ class MigrateGenerateCommand extends GeneratorCommand {
 		}
 
 		return [
-			'CLASS' => ucwords(camel_case($this->migrationName)),
+			'CLASS' => ucwords(Str::camel($this->migrationName)),
 			'UP'    => $up,
 			'DOWN'  => $down
 		];
