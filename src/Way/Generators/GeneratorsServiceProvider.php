a7c6a362e8bc0be81ac366706f06228ab26e7f61
kitloong <kitloong1008@hotmail.com>
2020-03-19 21:34:40 +0800

    Keep only registConfig

--- src/Way/Generators/GeneratorsServiceProvider.php
+++ src/Way/Generators/GeneratorsServiceProvider.php
@@ -1,23 +1,15 @@
 <?php namespace Way\Generators;
 
 use Illuminate\Support\ServiceProvider;
-use Way\Generators\Commands\ControllerGeneratorCommand;
-use Way\Generators\Commands\ModelGeneratorCommand;
-use Way\Generators\Commands\ResourceGeneratorCommand;
-use Way\Generators\Commands\SeederGeneratorCommand;
-use Way\Generators\Commands\PublishTemplatesCommand;
-use Way\Generators\Commands\ScaffoldGeneratorCommand;
-use Way\Generators\Commands\ViewGeneratorCommand;
-use Way\Generators\Commands\PivotGeneratorCommand;
 
-class GeneratorsServiceProvider extends ServiceProvider {
-
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+class GeneratorsServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
 
     /**
@@ -31,43 +23,14 @@ class GeneratorsServiceProvider extends ServiceProvider {
         ]);
     }
 
-	/**
-	 * Register the commands
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-        foreach([
-            'Model',
-            'View',
-            'Controller',
-            'Migration',
-            'Seeder',
-            'Pivot',
-            'Resource',
-            'Scaffold',
-            'Publisher'] as $command)
-        {
-            $this->{"register$command"}();
-        }
-
-        $this->registerConfig();
-	}
-
     /**
-     * Register the model generator
+     * Register the commands
+     *
+     * @return void
      */
-    protected function registerModel()
+    public function register()
     {
-        $this->app->singleton('generate.model', function($app)
-        {
-            $generator = $this->app->make('Way\Generators\Generator');
-
-            return new ModelGeneratorCommand($generator);
-        });
-
-        $this->commands('generate.model');
+        $this->registerConfig();
     }
 
     /**
@@ -75,140 +38,25 @@ class GeneratorsServiceProvider extends ServiceProvider {
      */
     public function registerConfig()
     {
-        $userConfigFile    = $this->app->configPath().'/generators.config.php';
+        $userConfigFile = $this->app->configPath().'/generators.config.php';
         $packageConfigFile = __DIR__.'/../../config/config.php';
-        $config            = $this->app['files']->getRequire($packageConfigFile);
+        $config = $this->app['files']->getRequire($packageConfigFile);
 
         if (file_exists($userConfigFile)) {
             $userConfig = $this->app['files']->getRequire($userConfigFile);
-            $config     = array_replace_recursive($config, $userConfig);
+            $config = array_replace_recursive($config, $userConfig);
         }
 
         $this->app['config']->set('generators.config', $config);
     }
 
     /**
-     * Register the view generator
+     * Get the services provided by the provider.
+     *
+     * @return array
      */
-    protected function registerView()
+    public function provides()
     {
-        $this->app->singleton('generate.view', function($app)
-        {
-            $generator = $this->app->make('Way\Generators\Generator');
-
-            return new ViewGeneratorCommand($generator);
-        });
-
-        $this->commands('generate.view');
+        return array();
     }
-
-    /**
-     * Register the controller generator
-     */
-    protected function registerController()
-    {
-        $this->app->singleton('generate.controller', function($app)
-        {
-            $generator = $this->app->make('Way\Generators\Generator');
-
-            return new ControllerGeneratorCommand($generator);
-        });
-
-        $this->commands('generate.controller');
-    }
-
-    /**
-     * Register the migration generator
-     */
-    protected function registerMigration()
-    {
-        $this->app->singleton('generate.migration', function($app)
-        {
-            return $this->app->make('Way\Generators\Commands\MigrationGeneratorCommand');
-        });
-
-        $this->commands('generate.migration');
-    }
-
-    /**
-     * Register the seeder generator
-     */
-    protected function registerSeeder()
-    {
-        $this->app->singleton('generate.seeder', function($app)
-        {
-            $generator = $this->app->make('Way\Generators\Generator');
-
-            return new SeederGeneratorCommand($generator);
-        });
-
-        $this->commands('generate.seeder');
-    }
-
-    /**
-     * Register the pivot generator
-     */
-    protected function registerPivot()
-    {
-        $this->app->singleton('generate.pivot', function($app)
-        {
-            return new PivotGeneratorCommand;
-        });
-
-        $this->commands('generate.pivot');
-    }
-
-    /**
-     * Register the resource generator
-     */
-    protected function registerResource()
-    {
-        $this->app->singleton('generate.resource', function($app)
-        {
-            $generator = $this->app->make('Way\Generators\Generator');
-
-            return new ResourceGeneratorCommand($generator);
-        });
-
-        $this->commands('generate.resource');
-    }
-
-    /**
-     * register command for publish templates
-     */
-    public function registerpublisher()
-    {
-        $this->app->singleton('generate.publish-templates', function($app)
-        {
-            return new publishtemplatescommand;
-        });
-
-        $this->commands('generate.publish-templates');
-    }
-
-    /**
-     * register scaffold command
-     */
-    public function registerScaffold()
-    {
-        $this->app->singleton('generate.scaffold', function($app)
-        {
-            return new ScaffoldGeneratorCommand;
-        });
-
-        $this->commands('generate.scaffold');
-    }
-
-
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
 }
