39e310099f0cde94abe54ebf39547e889f1823b4
kitloong <kitloong1008@hotmail.com>
2020-03-19 22:42:31 +0800

    Cleaned

--- src/config/config.php
+++ src/config/config.php
@@ -8,20 +8,8 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-    'model_template_path' => base_path('vendor/kitloong/laravel-migrations-generator/src/Way/Generators/templates/model.txt'),
-
-    'scaffold_model_template_path' => base_path('vendor/kitloong/laravel-migrations-generator/src/Way/Generators/templates/scaffolding/model.txt'),
-
-    'controller_template_path' => base_path('vendor/kitloong/laravel-migrations-generator/src/Way/Generators/templates/controller.txt'),
-
-    'scaffold_controller_template_path' => base_path('vendor/kitloong/laravel-migrations-generator/src/Way/Generators/templates/scaffolding/controller.txt'),
 
     'migration_template_path' => base_path('vendor/kitloong/laravel-migrations-generator/src/Way/Generators/templates/migration.txt'),
-
-    'seed_template_path' => base_path('vendor/kitloong/laravel-migrations-generator/src/Way/Generators/templates/seed.txt'),
-
-    'view_template_path' => base_path('vendor/kitloong/laravel-migrations-generator/src/Way/Generators/templates/view.txt'),
-
 
     /*
     |--------------------------------------------------------------------------
@@ -29,14 +17,7 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-    'model_target_path'   => app_path(),
-
-    'controller_target_path'   => app_path('Http/Controllers'),
 
     'migration_target_path'   => base_path('database/migrations'),
-
-    'seed_target_path'   => base_path('database/seeds'),
-
-    'view_target_path'   => base_path('resources/views')
 
 ];
