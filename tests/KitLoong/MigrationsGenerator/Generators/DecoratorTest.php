d3d5dfa2709ee2336c5ee7d19f8e6531cf26d6cf
kitloong <kitloong1008@hotmail.com>
2020-03-31 11:36:09 +0800

    Added UT

---
+++ tests/KitLoong/MigrationsGenerator/Generators/DecoratorTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liow.kitloong
+ * Date: 2020/03/29
+ * Time: 14:09
+ */
+
+namespace KitLoong\MigrationsGenerator\Generators;
+
+use Orchestra\Testbench\TestCase;
+
+class DecoratorTest extends TestCase
+{
+    public function testColumnDefaultToString()
+    {
+        /** @var Decorator $decorator */
+        $decorator = resolve(Decorator::class);
+        $result = $decorator->columnDefaultToString('string with " !@#$%^^&*()_+ \' quotes');
+        $this->assertSame('\'string with \" !@#$%^^&*()_+ \\\\\\\\\\\' quotes\'', $result);
+    }
+
+    public function testDecorate()
+    {
+        /** @var Decorator $decorator */
+        $decorator = resolve(Decorator::class);
+        $result = $decorator->decorate('method', []);
+        $this->assertSame('method', $result);
+
+        $result = $decorator->decorate('method', ['arg1', 'arg2']);
+        $this->assertSame('method(arg1, arg2)', $result);
+    }
+
+    public function testAddSlash()
+    {
+        /** @var Decorator $decorator */
+        $decorator = resolve(Decorator::class);
+        $result = $decorator->addSlash("Content's");
+        $this->assertSame("Content\'s", $result);
+    }
+}
