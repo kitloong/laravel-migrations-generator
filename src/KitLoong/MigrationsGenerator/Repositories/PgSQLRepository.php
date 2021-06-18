94c6e3141094ed236bfb54ff8c3d6dd209533bf5
kitloong <kitloong1008@hotmail.com>
2021-06-18 23:58:36 +0800

    Format

--- src/KitLoong/MigrationsGenerator/Repositories/PgSQLRepository.php
+++ src/KitLoong/MigrationsGenerator/Repositories/PgSQLRepository.php
@@ -18,20 +18,20 @@ class PgSQLRepository
 
         $column = $setting->getConnection()
             ->select("SELECT
-    pg_catalog.format_type(a.atttypid, a.atttypmod) as \"datatype\"
-FROM
-    pg_catalog.pg_attribute a
-WHERE
-    a.attnum > 0
-    AND NOT a.attisdropped
-    AND a.attrelid = (
-        SELECT c.oid
-        FROM pg_catalog.pg_class c
-            LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-        WHERE c.relname ~ '^(${table})$'
-            AND pg_catalog.pg_table_is_visible(c.oid)
-    )
-    AND a.attname='${columnName}'");
+                            pg_catalog.format_type(a.atttypid, a.atttypmod) as datatype
+                        FROM
+                            pg_catalog.pg_attribute a
+                        WHERE
+                            a.attnum > 0
+                            AND NOT a.attisdropped
+                            AND a.attrelid = (
+                                SELECT c.oid
+                                FROM pg_catalog.pg_class c
+                                    LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+                                WHERE c.relname ~ '^(${table})$'
+                                    AND pg_catalog.pg_table_is_visible(c.oid)
+                            )
+                            AND a.attname='${columnName}'");
         if (count($column) > 0) {
             return $column[0]->datatype;
         }
