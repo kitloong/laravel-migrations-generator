a5a05bc887a5579abe34f4e07756b5508fc27f5a
kitloong <kitloong1008@hotmail.com>
2021-07-04 23:49:04 +0800

    Removed

--- .github/docker/sqlsrv/db-init.sh
+++
@@ -1,7 +0,0 @@
-echo "Wait for the SQL Server to come up"
-sleep 30s
-echo "running set up script"
-# Run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q 'CREATE DATABASE migration'
-
-echo "DB created"
--- .github/docker/sqlsrv/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-# Start SQL Server, start the script to create/setup the DB
-chmod +x /db-init.sh
-/db-init.sh & /opt/mssql/bin/sqlservr
