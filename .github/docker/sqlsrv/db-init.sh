64dc1fc6549a67f1b1ba6f7acd1d880575819035
kitloong <kitloong1008@hotmail.com>
2021-07-04 20:59:39 +0800

    echo

--- .github/docker/sqlsrv/db-init.sh
+++ .github/docker/sqlsrv/db-init.sh
@@ -1,4 +1,4 @@
-wait for the SQL Server to come up
+echo "Wait for the SQL Server to come up"
 sleep 30s
 echo "running set up script"
 # Run the setup script to create the DB and the schema in the DB
