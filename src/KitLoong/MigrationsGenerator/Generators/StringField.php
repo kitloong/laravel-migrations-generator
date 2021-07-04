f560e6eb9f7e9f42fc969a1eb25756cbfb191e4e
kitloong <kitloong1008@hotmail.com>
2021-07-04 16:28:12 +0800

    Fixed sqlsrv text generation

--- src/KitLoong/MigrationsGenerator/Generators/StringField.php
+++ src/KitLoong/MigrationsGenerator/Generators/StringField.php
@@ -15,33 +15,57 @@ use KitLoong\MigrationsGenerator\MigrationMethod\ColumnName;
 use KitLoong\MigrationsGenerator\MigrationMethod\ColumnType;
 use KitLoong\MigrationsGenerator\MigrationsGeneratorSetting;
 use KitLoong\MigrationsGenerator\Repositories\PgSQLRepository;
+use KitLoong\MigrationsGenerator\Repositories\SQLSrvRepository;
 use KitLoong\MigrationsGenerator\Support\Regex;
 
 class StringField
 {
+    const SQLSRV_TEXT_TYPE = 'nvarchar';
+    const SQLSRV_TEXT_LENGTH = -1;
+
     private $collationModifier;
     private $charsetModifier;
     private $pgSQLRepository;
+    private $sqlSrvRepository;
     private $regex;
 
     public function __construct(
         CollationModifier $collationModifier,
         CharsetModifier $charsetModifier,
         PgSQLRepository $pgSQLRepository,
+        SQLSrvRepository $sqlSrvRepository,
         Regex $regex
     ) {
         $this->collationModifier = $collationModifier;
         $this->charsetModifier = $charsetModifier;
         $this->pgSQLRepository = $pgSQLRepository;
+        $this->sqlSrvRepository = $sqlSrvRepository;
         $this->regex = $regex;
     }
 
     public function makeField(string $tableName, array $field, Column $column): array
     {
-        if (($pgSQLEnum = $this->getPgSQLEnumValue($tableName, $column->getName())) !== '') {
-            $field['type'] = ColumnType::ENUM;
-            $field['args'][] = $pgSQLEnum;
-        } else {
+        switch (app(MigrationsGeneratorSetting::class)->getPlatform()) {
+            // It could be pgsql enum
+            case Platform::POSTGRESQL:
+                if (($pgSQLEnum = $this->getPgSQLEnumValue($tableName, $column->getName())) !== '') {
+                    $field['type'] = ColumnType::ENUM;
+                    $field['args'][] = $pgSQLEnum;
+                }
+                break;
+            // It could be sqlsrv text
+            case Platform::SQLSERVER:
+                $colDef = $this->sqlSrvRepository->getColumnDefinition($tableName, $column->getName());
+                if ($colDef->getType() === self::SQLSRV_TEXT_TYPE &&
+                    $colDef->getLength() === self::SQLSRV_TEXT_LENGTH) {
+                    $field['type'] = ColumnType::TEXT;
+                }
+                break;
+            default:
+        }
+
+        // Continue if type is `string`
+        if ($field['type'] === ColumnType::STRING) {
             if ($field['field'] === ColumnName::REMEMBER_TOKEN && $column->getLength() === 100 && !$column->getFixed()) {
                 $field['type'] = ColumnType::REMEMBER_TOKEN;
                 $field['field'] = null;
@@ -72,13 +96,11 @@ class StringField
 
     private function getPgSQLEnumValue(string $tableName, string $column): string
     {
-        if (app(MigrationsGeneratorSetting::class)->getPlatform() === Platform::POSTGRESQL) {
-            $definition = ($this->pgSQLRepository->getCheckConstraintDefinition($tableName, $column));
-            if (!empty($definition)) {
-                $enumValues = $this->regex->getTextBetweenAll($definition, "'", "'::");
-                if (!empty($enumValues)) {
-                    return "['".implode("', '", $enumValues)."']";
-                }
+        $definition = ($this->pgSQLRepository->getCheckConstraintDefinition($tableName, $column));
+        if (!empty($definition)) {
+            $enumValues = $this->regex->getTextBetweenAll($definition, "'", "'::");
+            if (!empty($enumValues)) {
+                return "['".implode("', '", $enumValues)."']";
             }
         }
         return '';
