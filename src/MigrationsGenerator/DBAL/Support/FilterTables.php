1497469e12e614e69573497d7eaf24c256b3601e
Warren Krewenki <19960+krewenki@users.noreply.github.com>
2022-05-13 12:26:07 -0300

    Update src/MigrationsGenerator/DBAL/Support/FilterTables.php
    Co-authored-by: Kit Loong <kitloong1008@gmail.com>

--- src/MigrationsGenerator/DBAL/Support/FilterTables.php
+++ src/MigrationsGenerator/DBAL/Support/FilterTables.php
@@ -40,7 +40,7 @@ trait FilterTables
         }
 
         // Schema name defined in Laravel framework.
-        $schema = $this->setting->getConnection()->getConfig('schema');
+        $schema = app(MigrationsGeneratorSetting::class)->getConnection()->getConfig('schema');
 
         $parts     = explode('.', $table);
         $namespace = $parts[0];
