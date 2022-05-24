c8dcb383bf40f3a3ac8f61012c511a33379e1867
Kit Loong <kitloong1008@gmail.com>
2022-05-24 22:58:12 +0800

    Fix phpstan error

--- tests/resources/database/migrations/general/2020_03_21_000000_expected_create_test_index_db_table.php
+++ tests/resources/database/migrations/general/2020_03_21_000000_expected_create_test_index_db_table.php
@@ -6,6 +6,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use KitLoong\MigrationsGenerator\Enum\Driver;
 use KitLoong\MigrationsGenerator\Support\CheckMigrationMethod;
