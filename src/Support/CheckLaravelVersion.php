6e0264b8fbfa4998bb9e9d2cb539e82e2fd66072
Kit Loong <kitloong1008@gmail.com>
2024-02-28 23:06:31 +0800

    Remove old code

--- src/Support/CheckLaravelVersion.php
+++ src/Support/CheckLaravelVersion.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\App;
 
 trait CheckLaravelVersion
 {
-    public function atLeastLaravel9(): bool
-    {
-        return $this->atLeastLaravelVersion('9.0');
-    }
-
     public function atLeastLaravel11(): bool
     {
         if (App::version() === '11.x-dev') {
