2c29c99aa938670635ea19569a568cb4f82378ad
Kit Loong <kitloong1008@gmail.com>
2024-08-04 11:04:22 +0800

    Merge tests

--- tests/Unit/Database/Models/PgSQL/PgSQLColumnTest.php
+++ tests/Unit/Database/Models/PgSQL/PgSQLColumnTest.php
@@ -8,12 +8,14 @@ use KitLoong\MigrationsGenerator\Repositories\PgSQLRepository;
 use KitLoong\MigrationsGenerator\Support\CheckLaravelVersion;
 use KitLoong\MigrationsGenerator\Tests\TestCase;
 use Mockery\MockInterface;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class PgSQLColumnTest extends TestCase
 {
     use CheckLaravelVersion;
 
-    public function testSpatialTypeNameWithDot(): void
+    #[DataProvider('spatialTypeNameProvider')]
+    public function testSpatialTypeName(string $type): void
     {
         $this->mock(PgSQLRepository::class, static function (MockInterface $mock): void {
             $mock->shouldReceive('getStoredDefinition');
@@ -22,7 +24,7 @@ class PgSQLColumnTest extends TestCase
         $column = new PgSQLColumn('table', [
             'name'           => 'column',
             'type_name'      => 'geography',
-            'type'           => 'extensions.geography(Point,4326)',
+            'type'           => $type,
             'collation'      => null,
             'nullable'       => false,
             'default'        => null,
@@ -40,30 +42,14 @@ class PgSQLColumnTest extends TestCase
         $this->assertSame(ColumnType::POINT, $column->getType());
     }
 
-    public function testSpatialTypeNameWithoutDot(): void
+    /**
+     * @return array<string, string[]>
+     */
+    public static function spatialTypeNameProvider(): array
     {
-        $this->mock(PgSQLRepository::class, static function (MockInterface $mock): void {
-            $mock->shouldReceive('getStoredDefinition');
-        });
-
-        $column = new PgSQLColumn('table', [
-            'name'           => 'column',
-            'type_name'      => 'geography',
-            'type'           => 'geography(Point,4326)',
-            'collation'      => null,
-            'nullable'       => false,
-            'default'        => null,
-            'auto_increment' => false,
-            'comment'        => null,
-        ]);
-
-        if ($this->atLeastLaravel11()) {
-            $this->assertSame(ColumnType::GEOGRAPHY, $column->getType());
-            $this->assertSame('point', $column->getSpatialSubType());
-            $this->assertSame(4326, $column->getSpatialSrID());
-            return;
-        }
-
-        $this->assertSame(ColumnType::POINT, $column->getType());
+        return [
+            'with dot'    => ['extensions.geography(Point,4326)'],
+            'without dot' => ['geography(Point,4326)'],
+        ];
     }
 }
